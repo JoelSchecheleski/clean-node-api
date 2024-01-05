851ffcdcfac355e92c2416b83203f38ace4c8088
Joel Schecheleski <joel.jsp@gmail.com>
2024-01-05 18:09:07 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+const config = {
+  roots: ['<rootDir>/src'],
+  clearMocks: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
+
+module.exports = config
